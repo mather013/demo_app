47e3aedf53f79323d98e0830fe686f99a03a8072
Mark Mather <mark.mather@footballpools.com>
2012-09-10 11:20:35 +0100

    added project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'dddb1899f1593caf36bb042c0f5176d62cecd6e2ab480bb936bb76e7ef340e19b89c44de4b477223b952183706d01b6e2226181aa2795caaec79ccfb9155432b'
