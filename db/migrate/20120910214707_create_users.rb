260cc0c5add136bfcae8c45981aec8cd72559ac5
Mark Mather <mathermark@hotmail.com>
2012-10-30 20:43:39 +0000

    Added name and email column to users table

--- db/migrate/20120910214707_create_users.rb
+++ db/migrate/20120910214707_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-
-      t.timestamps
-    end
+  def up
+    add_column :users, :name,  :string 
+    add_column :users, :email, :string
+  end
+  
+  def down
+    remove_column :users, :name
+    remove_column :users, :email    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120910211856) do
+ActiveRecord::Schema.define(:version => 20120910214707) do
 
   create_table "games", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(:version => 20120910211856) do
   create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
+    t.string   "email"
   end
 
 end
